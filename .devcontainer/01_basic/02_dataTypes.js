91c9709029def1907efab9284b14d224fae461f0
TusharSingh <123096917+Tusharsing@users.noreply.github.com>
2023-09-02 08:46:11 +0000

    datatyper , operators and comparsions

---
+++ .devcontainer/01_basic/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//all data typer//
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol
+object
+*/
+
+//***********Datatype Conversion******************\\
+// let num = 33;
+// let str ="33abc";
+// console.log(num);
+// console.log(typeof(num));
+// console.log(str);
+// console.log(typeof(str));
+
+
+
+//******comparision */
+
+
+// console.log(null >= 0);
+// console.log(null <=0);
+// console.log(null ===0);
+
+
+console.log(undefined >=0);
+console.log(undefined <=0);
+console.log(undefined ==0);
+
+
+
+
