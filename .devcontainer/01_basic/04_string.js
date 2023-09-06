d854277b0738c73776446e02d81abf67a5781727
TusharSingh <123096917+Tusharsing@users.noreply.github.com>
2023-09-06 05:29:21 +0000

    string and changes file name

---
+++ .devcontainer/01_basic/04_string.js
@@ -0,0 +1,18 @@
+ const str = new String("nam.enew");
+//  console.log(str.toUpperCase());
+//  console.log(str.indexOf('a'));
+//  console.log(str.charAt(5));
+//  console.log(str.substring(4,6));
+//  const nst = str.slice(0,3);
+//  console.log(nst);
+
+
+
+ /////replace////
+// const  repstr = str.replace('me','sSs');
+// console.log(repstr);
+///includes////
+// console.log(repstr.includes('m');
+
+//////split///////
+console.log(str.split('.'));
